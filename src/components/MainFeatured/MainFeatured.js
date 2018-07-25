acd062fd7c6b2b46febdbc6caa94c48b483fbd1d
Clayton Ingalls <github@zapbampow.com>
2018-07-25 10:02:18 -0400

    Bug fix: Updated MainFeatured.js to encode HTML character codes being returned in queries for titles with special characters in them (like &).

--- src/components/MainFeatured/MainFeatured.js
+++ src/components/MainFeatured/MainFeatured.js
@@ -4,14 +4,67 @@ import WideContainer from '../WideContainer';
 
 import styles from './MainFeatured.module.css'
 
+function htmlDecode(input){
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    // handle case of empty input
+    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+  }
+
 class MostRecent extends Component {
     render(){
-        const bgimage = `url(${this.props.data.featured_media.source_url})`
+        const bgimage = `url(${this.props.data.featured_media.source_url})`;
+
+        const title = htmlDecode(this.props.data.title);
 
         return (
             <div className={`${styles.featured} ${styles['most-recent']}`} style={{backgroundImage: bgimage}} >
                 <div className={styles['article-info-container']} >
-                    <h1 className={styles['featured-header']} >{this.props.data.title}</h1>
+                    <h1 className={styles['featured-header']} >{title}</h1>
+                    <div>
+                        <Link to={this.props.data.categories[0].link} className={styles['category-box-link']}>
+                            <span className={styles['category-box']}>{this.props.data.categories[0].name}</span>
+                        </Link>
+                        <span className={styles['featured-date']}>{this.props.data.date}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class SecondMostRecent extends Component {
+    render() {
+        const bgimage = `url(${this.props.data.featured_media.source_url})`
+
+        const title = htmlDecode(this.props.data.title);
+
+        return (
+            <div className={`${styles.featured} ${styles['second-most-recent']}`} style={{backgroundImage: bgimage}} >
+                <div className={styles['article-info-container']} >
+                    <h3 className={styles['featured-header']} >{title}</h3>
+                    <div>
+                        <Link to={this.props.data.categories[0].link} className={styles['category-box-link']}>
+                            <span className={styles['category-box']}>{this.props.data.categories[0].name}</span>
+                        </Link>
+                        <span className={styles['featured-date']}>{this.props.data.date}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class ThirdMostRecent extends Component {
+    render() {
+        const bgimage = `url(${this.props.data.featured_media.source_url})`
+
+        const title = htmlDecode(this.props.data.title);
+
+        return (
+            <div className={`${styles.featured} ${styles['third-most-recent']}`} style={{backgroundImage: bgimage}} >
+                <div className={styles['article-info-container']} >
+                    <h3 className={styles['featured-header']} >{title}</h3>
                     <div>
                         <Link to={this.props.data.categories[0].link} className={styles['category-box-link']}>
                             <span className={styles['category-box']}>{this.props.data.categories[0].name}</span>
@@ -25,48 +78,12 @@ class MostRecent extends Component {
 }
 
 
-// const MostRecent = (props) => 
-//     <div className={`${styles.featured} ${styles['most-recent']}`} style={{backgroundImage: bgimage }} >
-//         <div className={styles['article-info-container']} >
-//             <h1 className={styles['featured-header']} >{props.data.title}</h1>
-//             <div>
-//                 <Link to={props.data.categories[0].link} className={styles['category-box-link']}>
-//                     <span className={styles['category-box']}>{props.data.categories[0].name}</span>
-//                 </Link>
-//                 <span className={styles['featured-date']}>{props.data.date}</span>
-//             </div>
-//         </div>
-//     </div>
-
-
-const SecondMostRecent = (props) =>
-    <div className={`${styles.featured} ${styles['second-most-recent']}`} >
-        <div className={styles['article-info-container']} >
-            <h3 className={styles['featured-header']} >Article Title</h3>
-            <div>
-                <span className={styles['category-box']}>Category</span>
-                <span className={styles['featured-date']}>Date</span>
-            </div>
-        </div>
-    </div>
-
-const ThirdMostRecent = (props) =>
-    <div className={`${styles.featured} ${styles['third-most-recent']}`} >
-        <div className={styles['article-info-container']}>
-            <h3 className={styles['featured-header']}>Article Title</h3>
-            <div>
-                <span className={styles['category-box']} >Category</span>
-                <span className={styles['featured-date']}>Date</span>
-            </div>
-        </div>
-    </div>
-
 export default (props) => 
     <WideContainer className='bg-grey'>
         <div style={{ width:1140, height:450, display:'grid', gridTemplateColumns:'68% 32%', gridTemplateRows:'50% 50%', gridTemplateAreas:`"most-recent second-most-recent" "most-recent third-most-recent"` }}>
             <MostRecent data={props.data.edges[0].node} />
-            <SecondMostRecent />
-            <ThirdMostRecent />
+            <SecondMostRecent data={props.data.edges[1].node} />
+            <ThirdMostRecent data={props.data.edges[2].node} />
             <p>{props.data.edges[0].node.title}</p>
         </div>
     </WideContainer>
