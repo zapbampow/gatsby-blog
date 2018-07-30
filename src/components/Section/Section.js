b55a68c08807fde20dafd7e8f6dd486dd17b0da3
Clayton Ingalls <github@zapbampow.com>
2018-07-30 16:51:03 -0400

    Added .nvmrc file in hopes that it would cause the build to work.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -15,19 +15,22 @@ class Section extends Component {
             return (
                 <div key={index} className={styles['article-info-container']} >
                     <div className={styles['post-img']}>
-                    <Link to={node.slug} className={styles.linked}>
-                        <img src={node.featured_media.source_url} />
-                    </Link>
+                        <Link to={node.slug} className={styles.linked}>
+                            <img src={node.featured_media.source_url} />
+                        </Link>
                     </div>
+
                     <Link to={node.slug} className={styles.linked}>
                         <h3 className={styles['featured-header']} dangerouslySetInnerHTML={{__html: node.title}} > </h3>
                     </Link>
+
                     <div>
                         <Link to={node.categories[0].link} className={styles['category-box-link']}>
                             <span className={styles['category-box']}>{node.categories[0].name}</span>
                         </Link>
                         <span className={styles['featured-date']}>{node.date}</span>
                     </div>
+                    
                     <div className={styles.excerpt} dangerouslySetInnerHTML={{__html:excerpt}} ></div>
                 </div>
             )}
@@ -44,30 +47,6 @@ class Section extends Component {
 
                         <div className={styles['section-posts']}>
                             {content}
-
-                            {/* {this.props.data.edges.map(({node}, index) => {
-                                let excerpt = shortenExcerpt(node.excerpt);
-
-                                return (
-                                    <div key={index} className={styles['article-info-container']} >
-                                        <div className={styles['post-img']}>
-                                        <Link to={node.slug} className={styles.linked}>
-                                            <img src={node.featured_media.source_url} />
-                                        </Link>
-                                        </div>
-                                        <Link to={node.slug} className={styles.linked}>
-                                            <h3 className={styles['featured-header']} dangerouslySetInnerHTML={{__html: node.title}} > </h3>
-                                        </Link>
-                                        <div>
-                                            <Link to={node.categories[0].link} className={styles['category-box-link']}>
-                                                <span className={styles['category-box']}>{node.categories[0].name}</span>
-                                            </Link>
-                                            <span className={styles['featured-date']}>{node.date}</span>
-                                        </div>
-                                        <div className={styles.excerpt} dangerouslySetInnerHTML={{__html:excerpt}} ></div>
-                                    </div>
-                                )}
-                            )} */}
                         </div>
                 </div> 
                 </CenterContainer>
