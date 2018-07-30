d9f87b0ad5ee0282ccb7694b85d4ab70b51eef81
Clayton Ingalls <github@zapbampow.com>
2018-07-30 16:18:51 -0400

    It wasn't building in netlify. I moved the mapping of the data to be after the render, but before the return.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,17 +3,36 @@ import Link from 'gatsby-link';
 import WideContainer from '../WideContainer';
 import CenterContainer from '../CenterContainer';
 
-// let htmlDecode = null;
-// if (typeof document !== `undefined`) {
-//     htmlDecode = require('../../utils/htmlDecode')
-// }
-
 import shortenExcerpt from '../../utils/shortenExcerpt';
 
 import styles from './Section.module.css'
 
 class Section extends Component {
     render() {
+        var content = this.props.data.edges.map(({node}, index) => {
+            let excerpt = shortenExcerpt(node.excerpt);
+    
+            return (
+                <div key={index} className={styles['article-info-container']} >
+                    <div className={styles['post-img']}>
+                    <Link to={node.slug} className={styles.linked}>
+                        <img src={node.featured_media.source_url} />
+                    </Link>
+                    </div>
+                    <Link to={node.slug} className={styles.linked}>
+                        <h3 className={styles['featured-header']} dangerouslySetInnerHTML={{__html: node.title}} > </h3>
+                    </Link>
+                    <div>
+                        <Link to={node.categories[0].link} className={styles['category-box-link']}>
+                            <span className={styles['category-box']}>{node.categories[0].name}</span>
+                        </Link>
+                        <span className={styles['featured-date']}>{node.date}</span>
+                    </div>
+                    <div className={styles.excerpt} dangerouslySetInnerHTML={{__html:excerpt}} ></div>
+                </div>
+            )}
+        )
+
         return(
             <WideContainer className='home-section'>
                 <CenterContainer>
@@ -24,10 +43,9 @@ class Section extends Component {
                         </div>
 
                         <div className={styles['section-posts']}>
+                            {content}
 
-                            {this.props.data.edges.map(({node}, index) => {
-                                // let title = htmlDecode(node.title);
-                                // let excerpt = htmlDecode(shortenExcerpt(node.excerpt));
+                            {/* {this.props.data.edges.map(({node}, index) => {
                                 let excerpt = shortenExcerpt(node.excerpt);
 
                                 return (
@@ -49,7 +67,7 @@ class Section extends Component {
                                         <div className={styles.excerpt} dangerouslySetInnerHTML={{__html:excerpt}} ></div>
                                     </div>
                                 )}
-                            )}
+                            )} */}
                         </div>
                 </div> 
                 </CenterContainer>
