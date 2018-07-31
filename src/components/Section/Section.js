a3659c5e2c28b735650196ef929d9a4165b8b35b
Clayton Ingalls <github@zapbampow.com>
2018-07-30 20:28:18 -0400

    Another update to section.js to try to get this to work in production.

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -9,33 +9,6 @@ import styles from './Section.module.css'
 
 class Section extends Component {
     render() {
-        var content = this.props.data.edges.map(({node}, index) => {
-            let excerpt = shortenExcerpt(node.excerpt);
-    
-            return (
-                <div key={index} className={styles['article-info-container']} >
-                    <div className={styles['post-img']}>
-                        <Link to={node.slug} className={styles.linked}>
-                            <img src={node.featured_media.source_url} />
-                        </Link>
-                    </div>
-
-                    <Link to={node.slug} className={styles.linked}>
-                        <h3 className={styles['featured-header']} dangerouslySetInnerHTML={{__html: node.title}} > </h3>
-                    </Link>
-
-                    <div>
-                        <Link to={node.categories[0].link} className={styles['category-box-link']}>
-                            <span className={styles['category-box']}>{node.categories[0].name}</span>
-                        </Link>
-                        <span className={styles['featured-date']}>{node.date}</span>
-                    </div>
-                    
-                    <div className={styles.excerpt} dangerouslySetInnerHTML={{__html:excerpt}} ></div>
-                </div>
-            )}
-        )
-
         return(
             <WideContainer className='home-section'>
                 <CenterContainer>
@@ -46,7 +19,30 @@ class Section extends Component {
                         </div>
 
                         <div className={styles['section-posts']}>
-                            {content}
+
+                            {this.props.data.edges.map(({node}, index) => {
+                                let excerpt = shortenExcerpt(node.excerpt);
+                                
+                                return (
+                                    <div key={index} className={styles['article-info-container']} >
+                                        <div className={styles['post-img']}>
+                                        <Link to={node.slug} className={styles.linked}>
+                                            <img src={node.featured_media.source_url} />
+                                        </Link>
+                                        </div>
+                                        <Link to={node.slug} className={styles.linked}>
+                                            <h3 className={styles['featured-header']} dangerouslySetInnerHTML={{__html:node.title}} ></h3>
+                                        </Link>
+                                        <div>
+                                            <Link to={node.categories[0].link} className={styles['category-box-link']}>
+                                                <span className={styles['category-box']}>{node.categories[0].name}</span>
+                                            </Link>
+                                            <span className={styles['featured-date']}>{node.date}</span>
+                                        </div>
+                                        <div className={styles.excerpt}dangerouslySetInnerHTML={{__html:excerpt}}  ></div>
+                                    </div>
+                                )}
+                            )}
                         </div>
                 </div> 
                 </CenterContainer>
