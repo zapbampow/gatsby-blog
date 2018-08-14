1b44eba15f57bcc5f59f2a4cd489a2fc6dc9cc56
Clayton Ingalls <github@zapbampow.com>
2018-08-14 15:24:27 -0400

    fixed problems with last commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,9 +25,11 @@ module.exports = {
                * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
                * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
                */
-              baseUrl: "localhost/wp",
+              // baseUrl: "localhost/wp", //for use during development
+              baseUrl: "storynosh.com",
+
               // The protocol. This can be http or https.
-              protocol: "http",
+              protocol: "https",
               // Indicates whether the site is hosted on wordpress.com.
               // If false, then the assumption is made that the site is self hosted.
               // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
