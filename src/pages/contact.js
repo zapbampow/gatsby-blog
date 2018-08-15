70d9052977b3049f02371cd2c11a93110adbc076
Clayton Ingalls <github@zapbampow.com>
2018-08-15 15:52:22 -0400

    Trying again with netlify

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ return (<div style={{width:'100%', display:'flex', flexDirection:'column', align
             <h1>Contact Story Nosh!</h1>
 
             <form name="contact" method="POST" netlify>
-            <input type="hidden" name="contact" value="contact" />
+            <input type="hidden" name="form-name" value="contact" />
 
                 <p>
                     <label>Your Name: <input type="text" name="name" required style={{width:'100%'}}/></label>   
