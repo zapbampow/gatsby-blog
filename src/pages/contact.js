da47463f472bd05c4c446fb0ed31015d57916ebf
Clayton Ingalls <github@zapbampow.com>
2018-08-15 14:58:09 -0400

    Added contact form to contact page. And added footer.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 
-export default () => 
-    <div>
-        <h1>Contact Me!</h1>
+export default ({data}) => {
+return (<div style={{width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
+    <div style={{width:'30%'}}>
+            <h1>Contact Story Nosh!</h1>
 
-        <p>
-           <a href="mailto:myemail@example.com">My Email</a> 
-        </p>
+            <form name="contact" method="POST" netlify>
+                <p>
+                    <label>Your Name: <input type="text" name="name" required style={{width:'100%'}}/></label>   
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" required style={{width:'100%'}}/></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message" required style={{width:'100%'}}></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        
     </div>
+
+    <Footer data={data.footer} />
+</div>
+)}
+
+export const footerQuery = graphql`
+query footerQuery {
+    
+      
+      footer: allWordpressPost(sort: {fields: [date], order: DESC,}, limit: 3) {
+        edges {
+          node {
+            id
+            title
+            date (formatString: "MMM DD, YYYY")
+            slug            
+          }
+        }
+      }
+
+}
+`
